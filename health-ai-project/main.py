b0ead21a1edf498be6e15865eeb18e115bb13687
NicolasPedra <nicolas.rar@gmail.com>
2024-03-15 18:43:21 -0300

    post

--- health-ai-project/main.py
+++ health-ai-project/main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
+@app.route("/" , methods=['POST', 'GET'])
 def index():
-    return render_template("tela-consulta.html")
-
+    if request.method == "POST":
+        teste = ''
+        return render_template("tela-consulta.html")
+    else:
+        return render_template("tela-consulta.html", **locals())
+ 
 app.run(debug=True)
