9ac2a6585e73677aa4704237812c9054fcb10649
NicolasPedra <nicolas.rar@gmail.com>
2024-03-16 18:10:46 -0300

    implementação do gráfico

--- health-ai-project/static/script/tela-consulta.py
+++ health-ai-project/static/script/tela-consulta.py
@@ -1,11 +1,22 @@
 from pyscript import window, document
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
 
 def submit(event):
-    form = document.getElementById("form")
+    # form = document.getElementById("form")
 
-    if (form.checkValidity() == False):
-        event.preventDefault()
-        event.stopPropagation()
+    # if (form.checkValidity() == False):
+    #     event.preventDefault()
+    #     event.stopPropagation()
 
-    form.classList.add('was-validated')
+    # form.classList.add('was-validated')
+    exibirGrafico([10, 20, 45, 15, 5, 5])
+
+def exibirGrafico(valores: list[int]): 
+    plt.figure(1, figsize=(10, 4.7), layout='constrained')
+    categories = ['Idade', 'Histórico familiar', 'Nódulo Palpável', 'Tamanho do nódulo', 'BI-RADS USG', 'BI-RADS Mamografia']
+
+    plt.bar(categories, valores)
+    plt.show()
