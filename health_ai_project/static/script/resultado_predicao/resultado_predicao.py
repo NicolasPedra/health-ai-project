bcc43ffc2e0abe3ef9c53b6bbdc2fcd9d339b852
NicolasPedra <nicolas.rar@gmail.com>
2024-04-11 19:44:09 -0300

    refatoração dos métodos de gráfico

--- health_ai_project/static/script/resultado_predicao/resultado_predicao.py
+++ health_ai_project/static/script/resultado_predicao/resultado_predicao.py
@@ -5,53 +5,41 @@ import matplotlib.pyplot as plt
 def consultar_predicao(): 
     predicao_paciente_db = json.loads(document.getElementById("predicao_paciente_db").textContent)
 
-    print(predicao_paciente_db)
-    exibirGraficos([predicao_paciente_db['peso_idade'] * 100, 
-                    predicao_paciente_db['peso_historico_familiar'] * 100,
-                    predicao_paciente_db['peso_nodulo_palpavel'] * 100,
-                    predicao_paciente_db['peso_tamanho_nodulo'] * 100,
-                    predicao_paciente_db['peso_birads_usg'] * 100,
-                    predicao_paciente_db['peso_birads_mamografia'] * 100])
+    valores: list[int] = [predicao_paciente_db['peso_idade'], 
+                                predicao_paciente_db['peso_historico_familiar'],
+                                predicao_paciente_db['peso_nodulo_palpavel'],
+                                predicao_paciente_db['peso_tamanho_nodulo'],
+                                predicao_paciente_db['peso_birads_usg'],
+                                predicao_paciente_db['peso_birads_mamografia']]
+    
+    valores_negativos = obterValoresNegativos(valores)
+    valores_positivos = obterValoresPositivos(valores)
+    print(valores_negativos)
+    print(valores_positivos)
+    exibirGrafico(valores_negativos, valores_positivos)
 
-def voltar(event):
-    navegar('form', '/dados_paciente', 'GET')
-
-def exibirGraficos(valores: list[int]): 
-    categories = ['Idade', 'Histórico familiar', 'Nódulo Palpável', 'Tamanho do nódulo', 'BI-RADS USG', 'BI-RADS Mamografia']
+def exibirGrafico(valores_negativos: list[int], valores_positivos: list[int]): 
+    categorias = ['Idade', 'Histórico familiar', 'Nódulo Palpável', 'Tamanho do nódulo', 'BI-RADS USG', 'BI-RADS Mamografia']
     font_color = '#525252'
-    facecolor = '#eaeaf2'
     hfont = {'fontname':'DejaVu Sans'}
     color_red = '#fd625e'
     color_blue = '#01b8aa'
     titulo_negativo = "Benigno"
-    titulo_positivo = 'Maligno'
-    valores_negativos = []
-    valores_positivos = []
-
-    for valor in valores:
-        if (valor >= 0):
-            valores_positivos.append(valor)
-            valores_negativos.append(0)
-        else:
-            valores_positivos.append(0)
-            valores_negativos.append(valor)
-
+    titulo_positivo = 'Maligno' 
 
     fig, axes = plt.subplots(figsize=(14, 7), ncols=2,sharey=True)
     fig.canvas.set_window_title('')
     fig.tight_layout()
 
-    axes[0].barh(categories, valores_negativos, align='center', color=color_blue, zorder=10)
+    axes[0].barh(categorias, valores_negativos, align='center', color=color_blue, zorder=10)
     axes[0].set_title(titulo_negativo, fontsize=18, pad=15, color=color_blue, **hfont)
-    axes[1].barh(categories, valores_positivos, align='center', color=color_red, zorder=10)
+    axes[1].barh(categorias, valores_positivos, align='center', color=color_red, zorder=10)
     axes[1].set_title(titulo_positivo, fontsize=18, pad=15, color=color_red, **hfont)
     
-    # If you have positive numbers and want to invert the x-axis of the left plot
-    # To show data from highest to lowest
     plt.gca().invert_yaxis()
 
     axes[0].set_xticks([0, -10, -20, -30, -40, -50, -60, -70, -80, -90, -100])
-    axes[0].set(yticks=categories, yticklabels=categories)
+    axes[0].set(yticks=categorias, yticklabels=categorias)
     axes[0].yaxis.tick_left()
     axes[0].tick_params(axis='y', colors='white') # tick color
 
@@ -65,10 +53,26 @@ def exibirGraficos(valores: list[int]):
     plt.subplots_adjust(wspace=0, top=0.85, bottom=0.1, left=0.18, right=0.95)
     plt.show()
 
-def navegar(id_form, rota, metodo):
-    form = document.getElementById(id_form)
-    form.action = rota
-    form.method = metodo
-    form.submit()
+def obterValoresPositivos(valores: list[int]):
+    valores_positivos = []
+
+    for valor in valores:
+        if (valor >= 0):
+            valores_positivos.append(valor * 100)
+        else:
+            valores_positivos.append(0)
+
+    return valores_positivos
+
+def obterValoresNegativos(valores: list[int]):
+    valores_negativos = []
+
+    for valor in valores:
+        if (valor >= 0):
+            valores_negativos.append(0)
+        else:
+            valores_negativos.append(valor * 100)
+    
+    return valores_negativos
 
 consultar_predicao()
